59e0585993b217e49db3ecfa8b3e7b57b0aa0dd3
Sheldon Kemper <sheldonkemper@gmail.com>
2025-10-30 15:05:15 +0000

    databricks pat

--- terraform/provider.databricks.tf
+++ terraform/provider.databricks.tf
@@ -1,4 +1,9 @@
 provider "databricks" {
-  host  = var.databricks_workspace_url
-  token = var.databricks_token
+  host      = var.databricks_workspace_url
+  token     = var.databricks_token
+  auth_type = "pat"
+
+  azure_client_id                = null
+  azure_tenant_id                = null
+  azure_workspace_resource_id    = null
 }
